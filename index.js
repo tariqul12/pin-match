26922f67307a3536040bea539ed0edca89d5843b
tariqul12 <mshimul591@gmail.com>
2021-11-08 12:17:34 +0600

    pin match done

---
+++ index.js
@@ -0,0 +1,42 @@
+function getPin(){
+    const pin=Math.round(Math.random()*10000);
+    const pinString=pin+'';
+    if(pinString.length==4){
+        return pin;
+    }
+    else{
+        return getPin();
+    }
+}
+function generatePin(){
+    const pin=getPin();
+    document.getElementById('display-pin').value=pin;
+}
+document.getElementById('key-pad').addEventListener('click',function(event){
+    const number=event.target.innerText;
+    const calcInput=document.getElementById('typed-numbers');
+    if(isNaN(number)){
+        if(number=='C'){
+            calcInput.value='';
+        }
+    }
+    else{
+        const previousNumber=calcInput.value;
+        const newNumber=previousNumber+number;
+        calcInput.value=newNumber;
+    }
+});
+document.getElementById('submit').addEventListener('click',function(){
+    const pin=document.getElementById('display-pin').value;
+    const typeNumber=document.getElementById('typed-numbers').value;
+    const successMessage=document.getElementById('notify-success');
+    const failError=document.getElementById('notify-fail');
+    if(pin==typeNumber){
+        successMessage.style.display='block'
+        failError.style.display='none'
+    }
+    else{
+        successMessage.style.display='none'
+        failError.style.display='block';
+    }
+})
